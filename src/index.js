8b2b7990032b49227de71bf99cba010abf0fe937
Roman Pytalev <rpytalev@gmail.com>
2022-01-02 20:25:09 +0300

    feat: implement function that reverse digits of this number

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function reverse (n) {
-  
+    return n < 0 ? reverseNum(n) : reverseNum(n);
+
+    function reverseNum(num) {
+        let str = String(num);
+        if (num < 0) str = str.slice(1);
+        str.split('');
+        let newStr = '';
+    
+        for (let i = str.length; i--;) {
+            newStr = newStr + str[i];
+        }
+        return +newStr;
+    }
 }
